41d587d2026fdd9912d29034ab2fdc5ae0cfd744
amittaranto <amit.taranto@gmail.com>
2019-06-02 20:32:29 +0300

    added gif option by button

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -6,24 +6,28 @@ import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView question;
+    GifImageView gif_end;
     ListView answersList;
     ArrayAdapter<String> adapter;
     Server.Question currentQuestion;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         question = findViewById(R.id.text);
         answersList = findViewById(R.id.answersList);
         adapter = new ArrayAdapter<>(this,
@@ -59,5 +63,12 @@ public class MainActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void testFunc(View view) {
+
+        b = findViewById(R.id.gifbutton);
+        gif_end = findViewById(R.id.gif);
+        gif_end.setVisibility(View.VISIBLE);
     }
 }
